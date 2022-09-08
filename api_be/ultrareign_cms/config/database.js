2744e95bac5b7f61c06b5518bdd5bf7f4626daea
Sky Livingston <27288135+skypad123@users.noreply.github.com>
2022-09-08 21:36:15 +0800

    proper setup of strapi v4

---
+++ api_be/ultrareign_cms/config/database.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST'),
+        port: env('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: {
+          ca: fs.readFileSync(`./ca-certificate.crt`).toString(),
+        },
+      },
+      options: {}
+    },
+  },
+});
