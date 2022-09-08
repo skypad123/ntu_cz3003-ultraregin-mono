7765be92a4efffb33e661f172a1cabb2843a3c7f
Sky Livingston <27288135+skypad123@users.noreply.github.com>
2022-09-08 22:58:04 +0800

    fix jwt

--- api_be/ultrareign_cms/config/server.js
+++ api_be/ultrareign_cms/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST'),
-  port: env.int('PORT'),
-  // admin: {
-  //   auth: {
-  //     secret: env('ADMIN_JWT_SECRET',"0"),
-  //   },
-  // },
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ea5f80f79fd1a7d8c2a6cdb7b4d68116'),
+    },
+  },
 });
