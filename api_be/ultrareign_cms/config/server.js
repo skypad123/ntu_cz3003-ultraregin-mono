f0de0fbe749bb7b0545f920990b94f1bbc8c2e82
Sky Livingston <27288135+skypad123@users.noreply.github.com>
2022-11-04 22:50:22 +0800

    add test setup & production setup

--- api_be/ultrareign_cms/config/server.js
+++ api_be/ultrareign_cms/config/server.js
@@ -1,18 +1,18 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   url: env("PUBLIC_URL"),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-// });
-
-
-
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
-  app: { 
-    keys: env.array('APP_KEYS')
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env("PUBLIC_URL"),
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
+
+
+
+// module.exports = ({ env }) => ({
+//   proxy: true,
+//   url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
+//   app: { 
+//     keys: env.array('APP_KEYS')
+//   },
+// });
