50c0f9db5ba14cc50b562c24e3a9b065e1d5889e
Sky Livingston <27288135+skypad123@users.noreply.github.com>
2022-11-05 20:59:11 +0800

    fixed world-level composition and setup for test database

---
+++ api_be/ultrareign_cms/src/api/world-level-composition/controllers/world-level-composition.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * world-level-composition controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::world-level-composition.world-level-composition');
---
+++ api_be/ultrareign_cms/src/api/world-level-composition/routes/world-level-composition.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * world-level-composition router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::world-level-composition.world-level-composition');
